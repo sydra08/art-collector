69cbe228ddbccd7353a223a30f32b204de3c3ecb
github username <github email address>
2017-09-12 21:44:12 -0400

    add migrations for join tables

--- db/migrate/20170913010129_create_artworks.rb
+++ db/migrate/20170913010129_create_artworks.rb
@@ -2,8 +2,8 @@ class CreateArtworks < ActiveRecord::Migration
   def change
     create_table :artworks do |t|
       t.string :name
-      t.integer :artist_id 
-      t.integer :year
+      t.integer :artist_id
+      t.string :year
     end
   end
 end
--- db/migrate/20170913011917_create_collection_artworks.rb
+++ db/migrate/20170913011917_create_collection_artworks.rb
@@ -1,6 +1,6 @@
 class CreateCollectionArtworks < ActiveRecord::Migration
   def change
-    create_table: collection_artworks do |t|
+    create_table :collection_artworks do |t|
       t.integer :collection_id
       t.integer :artwork_id
     end
