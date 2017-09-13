7969593a5230ddf7aed256c18b1ff8ba6a18b318
github username <github email address>
2017-09-12 22:00:21 -0400

    add seed data for artists

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# create artists
+davinci = Artist.create(name: "Leonardo Da Vinci", birth_year: , style: "", location: "Italy", alive: false)
+vermeer = Artist.create(name: "Johannes Vermeer", birth_year: , style: "", location: "", alive: false)
+botticelli = Artist.create(name: "Sandro Botticelli", birth_year: , style: "", location: "", alive: false)
+vangogh = Artist.create(name: "Vincent Van Gogh", birth_year: , style: "", location: "", alive: false)
+whistler = Artist.create(name: "James Abbott McNeill Whistler", birth_year: , style: "", location: "", alive: false)
+klimt = Artist.create(name: "Gustav Klimt", birth_year: , style: "", location: "", alive: false)
+vaneyck = Artist.create(name: "Jan van Eyck", birth_year: , style: "", location: "", alive: false)
+bosch = Artist.create(name: "Hieronymus Bosch", birth_year: , style: "", location: "", alive: false)
+seurat = Artist.create(name: "Geroges Seurat", birth_year: , style: "", location: "", alive: false)
+picasso = Artist.create(name: "Pablo Picasso", birth_year: , style: "", location: "", alive: false)
+
+# create artworks
