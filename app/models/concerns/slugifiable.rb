5c1c66631afcaa09d4f7651a63bba48974d846f5
github username <schun08@gmail.com>
2017-09-15 21:31:16 -0400

    update user.slug to user username

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -11,5 +11,5 @@ module Slugifiable
       name.downcase.gsub(" ", "-")
     end
   end
-  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ActiveRecord::Base
   extend Slugifiable::ClassMethods
-  include Slugifiable::InstanceMethods
+  include Slugifiable::InstanceMethods #should i keep this if i override it?
 
   has_many :user_collections
   has_many :collections, through: :user_collections
   has_secure_password
   validates :username, :email, presence: true
+
+  def slug
+    username.downcase.gsub(" ", "-")
+  end
+
 end
