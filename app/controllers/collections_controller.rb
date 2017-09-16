5228115963c9fece2f85bf1a90732311172b39a1
github username <schun08@gmail.com>
2017-09-15 23:00:02 -0400

    created basic form for creating a new collection

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,11 +1,31 @@
 class CollectionsController < ApplicationController
 
   get '/collections/new' do
-    erb :'/collections/create_collection'
+    if logged_in?
+      erb :'/collections/create_collection'
+    else
+      # you must be logged in to do that
+      redirect '/login'
+    end
+  end
+
+  post '/collections' do
+    @collection = Collection.new(name: params[:collection][:name], artwork_ids: params[:collection][:artwork_ids])
+
+    # create new artwork and artist
+    @artwork = Artwork.find_or_initialize_by(name: params[:artwork][:name], year: params[:artwork][:year])
+    @artwork.artist = Artist.find_or_create_by(name: params[:artwork][:artist])
+    @artwork.save
+
+    # add artwork to the collection
+    @collection.artworks << @artwork
+    @collection.save
+    binding.pry
+    redirect to "/collections/#{@collection.id}"
   end
 
   get '/collections/:id' do
-    @collection = Collection.find_by_slug(params[:id])
+    @collection = Collection.find(params[:id])
     erb :'/collections/show_collection'
   end
 
