414a8e37f904e4177a8d06159fa503495f53ae07
github username <schun08@gmail.com>
2017-09-16 17:31:08 -0400

    create views for adding and editing artist. update artist show view

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,9 +4,30 @@ class ArtistsController < ApplicationController
     erb :'/artists/artists'
   end
 
+  get '/artists/new' do
+    erb :'/artists/new_artist'
+  end
+
+  post '/artists' do
+    #create new artist here if it doesn't already exist
+    redirect to "/artists/#{@artist.slug}"
+  end
+
   get '/artists/:slug' do
     @artist = Artist.find_by_slug(params[:slug])
     erb :'/artists/show_artist'
+  end
+
+  patch '/artists/:slug' do
+    # edit artist
+    @artist = Artist.find_by_slug(params[:slug])
+
+    redirect to "/artists/#{@artist.slug}"
+  end
+
+  get '/artists/:slug/edit' do
+    @artist = Artist.find_by_slug(params[:slug])
+    erb :'/artists/edit_artist'
   end
 
 end
