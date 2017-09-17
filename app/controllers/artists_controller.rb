529e98103480f9246ada5842b9930b828cc22867
github username <schun08@gmail.com>
2017-09-17 16:00:34 -0400

    remove add/edit artwork and artist features

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,76 +3,76 @@ class ArtistsController < ApplicationController
   get '/artists' do
     erb :'/artists/artists'
   end
-
-  get '/artists/new' do
-    if logged_in?#users should only be allowed to access this if they are logged in
-      erb :'/artists/new_artist'
-    else
-      # flash message - you must be logged in to create a new artist
-      redirect to '/artists'
-    end
-  end
-
-  post '/artists' do
-    @artist = Artist.find_by(name: params[:name])
-    if @artist == nil
-      binding.pry
-      @artist = Artist.new(name: params[:name], birth_year: params[:year], location: params[:location], movement: params[:movement], alive: params[:alive])
-      if params[:artwork].empty?
-        @artwork = Artwork.find_or_create_by(name: params[:artwork][:name], year: params[:artwork][:year])
-        @artist.artworks << @artwork
-      end
-      if @artist.save
-        # what does this do if it doesn't save?
-        redirect to "/artists/#{@artist.slug}"
-      end
-    else
-      # flash msg - that artist already exists
-      # artist page or artists page?
-      redirect to '/artists'
-    end
-  end
+  #
+  # get '/artists/new' do
+  #   if logged_in?#users should only be allowed to access this if they are logged in
+  #     erb :'/artists/new_artist'
+  #   else
+  #     # flash message - you must be logged in to create a new artist
+  #     redirect to '/artists'
+  #   end
+  # end
+  #
+  # post '/artists' do
+  #   @artist = Artist.find_by(name: params[:name])
+  #   if @artist == nil
+  #     binding.pry
+  #     @artist = Artist.new(name: params[:name], birth_year: params[:year], location: params[:location], movement: params[:movement], alive: params[:alive])
+  #     if params[:artwork].empty?
+  #       @artwork = Artwork.find_or_create_by(name: params[:artwork][:name], year: params[:artwork][:year])
+  #       @artist.artworks << @artwork
+  #     end
+  #     if @artist.save
+  #       # what does this do if it doesn't save?
+  #       redirect to "/artists/#{@artist.slug}"
+  #     end
+  #   else
+  #     # flash msg - that artist already exists
+  #     # artist page or artists page?
+  #     redirect to '/artists'
+  #   end
+  # end
 
   get '/artists/:slug' do
     @artist = Artist.find_by_slug(params[:slug])
     erb :'/artists/show_artist'
   end
-
-  patch '/artists/:slug' do
-    @artist = Artist.find_by_slug(params[:slug])
-    # is there a better way to do this?
-    if !params[:artist][:name].empty?
-      @artist.name = params[:artist][:name]
-    end
-
-    if !params[:artist][:year].empty?
-      @artist.birth_year = params[:artist][:year]
-    end
-
-    if !params[:artist][:location].empty?
-      @artist.location = params[:artist][:location]
-    end
-
-    if !params[:artist][:movement].empty?
-      @artist.movement = params[:artist][:movement]
-    end
-
-    if !params[:artist][:alive].empty?
-      @artist.alive = params[:artist][:alive]
-    end
-
-    @artist.save
-    redirect to "/artists/#{@artist.slug}"
-  end
-
-  get '/artists/:slug/edit' do
-    if logged_in?
-      @artist = Artist.find_by_slug(params[:slug])
-      erb :'/artists/edit_artist'
-    else
-      # flash msg - that they need to be logged in to do that
-      redirect to "/artists/#{@artist.slug}"
-    end
-  end
+  #
+  # patch '/artists/:slug' do
+  #   @artist = Artist.find_by_slug(params[:slug])
+  #   # is there a better way to do this?
+  #   if !params[:artist][:name].empty?
+  #     @artist.name = params[:artist][:name]
+  #   end
+  #
+  #   if !params[:artist][:year].empty?
+  #     @artist.birth_year = params[:artist][:year]
+  #   end
+  #
+  #   if !params[:artist][:location].empty?
+  #     @artist.location = params[:artist][:location]
+  #   end
+  #
+  #   if !params[:artist][:movement].empty?
+  #     @artist.movement = params[:artist][:movement]
+  #   end
+  #
+  #   if !params[:artist][:alive].empty?
+  #     @artist.alive = params[:artist][:alive]
+  #   end
+  #
+  #   @artist.save
+  #   redirect to "/artists/#{@artist.slug}"
+  # end
+  #
+  # get '/artists/:slug/edit' do
+  #   if logged_in?
+  #     @artist = Artist.find_by_slug(params[:slug])
+  #     erb :'/artists/edit_artist'
+  #   else
+  #     # flash msg - that they need to be logged in to do that
+  #     redirect to "/artists/#{@artist.slug}"
+  #   end
+  # end
 
 end
--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -4,74 +4,76 @@ class ArtworksController < ApplicationController
     erb :'/artworks/artworks'
   end
 
-  get '/artworks/new' do
-    if logged_in?
-      erb :'/artworks/new_artwork'
-    else
-      # you must be logged in to do that
-      redirect to '/artworks'
-    end
-  end
-
-  post '/artworks' do
-    binding.pry
-    @artwork = Artwork.find_by(name: params[:name])
-    if @artwork == nil
-      @artwork = Artwork.new(name: params[:name], year: params[:year])
-      if !params[:artist].empty?
-        @artist = Artist.find_or_create_by(name: params[:artist])
-      end
-      if @artwork.save
-        @artist.artworks << @artwork
-        redirect to "/artworks/#{@artwork.slug}"
-      else
-        # there was an error processing your request
-        redirect to '/artworks/new'
-      end
-    else
-      # that artwork already exists
-      redirect to "/artworks/#{@artwork.slug}"
-    end
-  end
-
-  get '/artworks/:slug' do
-    @artwork = Artwork.find_by_slug(params[:slug])
+  get '/artworks/:id' do
+    @artwork = Artwork.find(params[:id])
     erb :'/artworks/show_artwork'
   end
 
-  patch '/artworks/:slug' do
-    @artwork = Artwork.find_by_slug(params[:slug])
-    binding.pry
-    if !params[:name].empty?
-      @artwork.name = params[:name]
-    end
+  # get '/artworks/new' do
+  #   if logged_in?
+  #     erb :'/artworks/new_artwork'
+  #   else
+  #     # you must be logged in to do that
+  #     redirect to '/artworks'
+  #   end
+  # end
+  #
+  # post '/artworks' do
+  #   binding.pry
+  #   @artwork = Artwork.find_by(name: params[:name])
+  #   if @artwork == nil
+  #     @artwork = Artwork.new(name: params[:name], year: params[:year])
+  #     if !params[:artist].empty?
+  #       @artist = Artist.find_or_create_by(name: params[:artist])
+  #     end
+  #     if @artwork.save
+  #       @artist.artworks << @artwork
+  #       redirect to "/artworks/#{@artwork.slug}"
+  #     else
+  #       # there was an error processing your request
+  #       redirect to '/artworks/new'
+  #     end
+  #   else
+  #     # that artwork already exists
+  #     redirect to "/artworks/#{@artwork.slug}"
+  #   end
+  # end
 
-    if !params[:year].empty?
-      @artwork.year = params[:year]
-    end
 
-    if params[:artist] != @artwork.artist.name
-      @artist = Artist.find_or_create_by(name: params[:artist])
-      @artist.artworks << @artwork
-    end
 
-    if @artwork.save
-      redirect to "/artworks/#{@artwork.slug}"
-    else
-      # there was an error processing your request
-      redirect to "/artworks/#{@artwork.slug}"
-    end
+  # patch '/artworks/:slug' do
+  #   @artwork = Artwork.find_by_slug(params[:slug])
+  #   binding.pry
+  #   if !params[:name].empty?
+  #     @artwork.name = params[:name]
+  #   end
+  #
+  #   if !params[:year].empty?
+  #     @artwork.year = params[:year]
+  #   end
+  #
+  #   if params[:artist] != @artwork.artist.name
+  #     @artist = Artist.find_or_create_by(name: params[:artist])
+  #     @artist.artworks << @artwork
+  #   end
+  #
+  #   if @artwork.save
+  #     redirect to "/artworks/#{@artwork.slug}"
+  #   else
+  #     # there was an error processing your request
+  #     redirect to "/artworks/#{@artwork.slug}"
+  #   end
+  #
+  # end
+  #
+  # get '/artworks/:slug/edit' do
+  #   @artwork = Artwork.find_by_slug(params[:slug])
+  #   if logged_in?
+  #     erb :'/artworks/edit_artwork'
+  #   else
+  #     # you must be logged in to do that
+  #     redirect to "/artworks/#{@artwork.slug}"
+  #   end
+  # end
 
-  end
-
-  get '/artworks/:slug/edit' do
-    @artwork = Artwork.find_by_slug(params[:slug])
-    if logged_in?
-      erb :'/artworks/edit_artwork'
-    else
-      # you must be logged in to do that
-      redirect to "/artworks/#{@artwork.slug}"
-    end
-  end
-  
 end
