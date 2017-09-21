aeeb912b002ab40614d009926340701eae26b438
github username <schun08@gmail.com>
2017-09-20 21:41:20 -0400

    add sort by artwork count method to Artist model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -5,4 +5,9 @@ class Artist < ActiveRecord::Base
   has_many :artworks
   validates :name, presence: true
     validates_uniqueness_of :name, :case_sensitive => false, :message => "An artist with that name already exists"
+
+  def self.sort_by_artwork_count
+    self.all.sort{|a,b| b.artworks.count <=> a.artworks.count}
+  end
+
 end
