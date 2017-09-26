abd8e6c6171ea984ffae7b1003d3bd3ddd1c16de
github username <schun08@gmail.com>
2017-09-25 21:45:54 -0400

    refactor post /artworks. update error messaging for artworks controller

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -6,52 +6,38 @@ class ArtworksController < ApplicationController
 
   post '/artworks' do
     if user_collection_valid?
-      if !params[:artwork_id].empty?
+      # don't need to check if field is empty bc form has required field
+      @artwork = Artwork.find_or_create_by(name: params[:artwork][:name]) do |a|
+        a.year = params[:artwork][:year] unless params[:artwork][:year] == ""
+      end
 
-        @artwork = Artwork.find(params[:artwork_id])
-        if !current_collection.artworks.include?(@artwork)
-          current_collection.artworks << @artwork
+      if @artwork.valid? && !current_collection.artworks.include?(@artwork)
 
-          flash[:message] = "#{@artwork.name} was successfully added to your collection"
-          redirect to "/collections/#{current_collection.id}"
-        else
-          flash[:message] = "#{@artwork.name} is already in your collection"
-          redirect to "/collections/#{current_collection.id}"
+        if @artwork.artist_id == nil && !params[:artwork][:artist].empty?
+          # only adds an artist if the artwork doesn't have one already and the artist param isn't empty
+          @artist = Artist.find_or_create_by(name: params[:artwork][:artist])
+          if @artist.valid?
+            @artist.artworks << @artwork
+          end
         end
 
-      elsif !params[:artwork].empty?
-        @artwork = Artwork.find_or_create_by(name: params[:artwork][:name]) do |a|
-          a.year = params[:artwork][:year] unless params[:artwork][:year] == ""
-        end
+        current_collection.artworks << @artwork
+        flash[:message] = "#{@artwork.name} was successfully added to your collection"
+        redirect to "/collections/#{current_collection.id}"
 
-        if @artwork.valid?
-
-          if @artwork.artist_id == nil && !params[:artwork][:artist].empty?
-            # only adds an artist if the artwork doesn't have one already and the artist param isn't empty
-            @artist = Artist.find_or_create_by(name: params[:artwork][:artist])
-            if @artist.valid?
-              @artist.artworks << @artwork
-            end
-          end
-
-          if !current_collection.artworks.include?(@artwork)
-            current_collection.artworks << @artwork
-            flash[:message] = "#{@artwork.name} was successfully added to your collection"
-            redirect to "/collections/#{current_collection.id}"
-          else
-            flash[:message] = "#{@artwork.name} is already in your collection"
-            redirect to '/artworks/new'
-          end
-
+      else
+        if !@artwork.errors[:name][0].nil?
+          flash[:message] = "Error: #{@artwork.errors[:name][0]}"
+          redirect to '/artworks/new'
         else
-          flash[:message] = "There was an error processing your request: #{@artwork.errors[:name][0]}"
+          flash[:message] = "Error: #{@artwork.name} is already in your collection"
           redirect to '/artworks/new'
         end
       end
 
     else
       # if you try to edit someone else's collection
-      flash[:message] = "You cannot edit someone else's collection"
+      flash[:message] = "Error: You cannot edit someone else's collection"
       redirect to "/users/#{current_user.slug}"
     end
   end
@@ -62,11 +48,11 @@ class ArtworksController < ApplicationController
         @available_artworks =  Artwork.all.select{|a| !current_collection.artworks.include?(a)}
         erb :'/artworks/new_artwork'
       else
-        flash[:message] = "You cannot edit someone else's collection"
+        flash[:message] = "Error: You cannot edit someone else's collection"
         redirect to "/users/#{current_user.slug}"
       end
     else
-      flash[:message] = "You must be logged in to edit a collection"
+      flash[:message] = "Error: You must be logged in to edit a collection"
        redirect to '/'
     end
   end
@@ -85,7 +71,7 @@ class ArtworksController < ApplicationController
       flash[:message] = "#{@artwork.name} successfully removed from your collection"
       redirect to "/collections/#{current_collection.id}"
     else
-      flash[:message] = "You cannot edit someone else's collection"
+      flash[:message] = "Error: You cannot edit someone else's collection"
       redirect to "/users/#{current_user.slug}"
     end
   end
