83b9ee3502fa73bac796c103f1eaa900028a5487
github username <schun08@gmail.com>
2017-09-29 18:38:22 -0400

    fix spacing

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,7 +4,7 @@ class Artist < ActiveRecord::Base
 
   has_many :artworks
   validates :name, presence: true
-    validates_uniqueness_of :name, :case_sensitive => false, :message => "An artist with that name already exists"
+  validates_uniqueness_of :name, :case_sensitive => false, :message => "An artist with that name already exists"
 
   def self.sort_by_artwork_count
     self.all.sort{|a,b| b.artworks.count <=> a.artworks.count}
